7b5028e664ce98607221892650a8c6d3ee706655
Renaud Grishenko <gray.fawkes@outlook.com>
2020-10-09 23:27:08 +0200

    Implémentation des fichiers bdpm #4

--- src/components/Updates.vue
+++ src/components/Updates.vue
@@ -1,6 +1,10 @@
 <template>
 	<div id="updates" class="container">
-			<button class="btn btn-info" v-on:click="updateBDPM()">Update BDPM</button>
+			<button class="btn btn-info" v-on:click="updateBDPM('cis')">Update CIS</button>
+			<button class="btn btn-info" v-on:click="updateBDPM('cip')">Update CIP</button>
+			<button class="btn btn-info" v-on:click="updateBDPM('compo')">Update Compo</button>
+			<button class="btn btn-info" v-on:click="updateBDPM('gener')">Update Gener</button>
+			<button class="btn btn-info" v-on:click="updateBDPM('cpd')">Update CPD</button>
 	</div>
 </template>
 <script>
@@ -14,9 +18,10 @@ export default {
 		// this.updateBDPM()
 	},
 	methods: {
-		updateBDPM () {
+		updateBDPM (file) {
+			var url = '/api/updates/' + file
 			// Call API
-			axios.put('/api/updates').then(
+			axios.put(url).then(
 				result => {
 					// console.log(result.data)
 					// this.clients = result.data
